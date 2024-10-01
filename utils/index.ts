e19e6daf6481c3ea83a089268849f58c33698c54
Prasit Sahu <prasitsahu4@gmail.com>
2024-10-02 01:27:05 +0530

    added generateUID utility function

--- utils/index.ts
+++ utils/index.ts
@@ -7,3 +7,14 @@ export function withTimeStamp(update: Update): UpdateDoc {
     data: update,
   };
 }
+
+export function generateUID(rand: number = Math.floor(Math.random() * 10)) {
+  const randAlphs = () =>
+    [0, 0, 0, 0].map((alph) => Math.floor(Math.random() * (122 - 97 + 1)) + 97);
+
+  return `${randAlphs()
+    .map((alph) => String.fromCharCode(alph))
+    .join("")}${Math.floor(Math.random() * 1000)}${Math.floor(
+    Math.random() * 1000
+  )}-${Date.now()}r${rand}-${Math.random().toString(36).substr(2, 9)}`;
+}
