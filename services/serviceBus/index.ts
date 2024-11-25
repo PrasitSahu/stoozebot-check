855c07aa46013c7534e61ea1b563afabad0eff60
Prasit Sahu <prasitsahu4@gmail.com>
2024-11-25 23:58:34 +0530

    feat: add servicebus queue receive and closeClient options

--- services/serviceBus/index.ts
+++ services/serviceBus/index.ts
@@ -1,6 +1,8 @@
 import {
+  ReceiveMessagesOptions,
   ServiceBusClient,
   ServiceBusMessage,
+  ServiceBusReceivedMessage,
   ServiceBusReceiver,
   ServiceBusSender,
 } from "@azure/service-bus";
@@ -10,11 +12,13 @@ export default class QueueService<Message> {
   private queueClient: ServiceBusClient;
   private sender: ServiceBusSender;
   private receiver: ServiceBusReceiver;
+  private static queueClients: ServiceBusClient[] = [];
 
   constructor(private queueConnString: string, private queueName: string) {
     this.queueClient = new ServiceBusClient(this.queueConnString);
     this.sender = this.queueClient.createSender(this.queueName);
     this.receiver = this.queueClient.createReceiver(this.queueName);
+    QueueService.queueClients.push(this.queueClient);
   }
 
   public async sendMessages(messages: Message[]) {
@@ -36,6 +40,28 @@ export default class QueueService<Message> {
       await this.sender.sendMessages(batch);
     } catch (error) {
       console.error(error);
+    }
+  }
+  public async recieveMessages(
+    count: number,
+    receiveMode: "peekLock" | "receiveAndDelete",
+    options?: ReceiveMessagesOptions
+  ) {
+    this.receiver.receiveMode = receiveMode;
+    return await this.receiver.receiveMessages(count, options);
+  }
+
+  public async completeMessage(message: ServiceBusReceivedMessage) {
+    await this.receiver.completeMessage(message);
+  }
+
+  public async closeClient() {
+    await this.queueClient.close();
+  }
+
+  public static async closeAllClients() {
+    for (let client of QueueService.queueClients) {
+      await client.close();
     }
   }
 }
